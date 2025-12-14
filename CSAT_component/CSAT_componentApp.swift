70e9ec01a76b102b8d19578cb6b46c86fa6dd0e4
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-12-14 18:03:58 +0530

    initial commit

---
+++ CSAT_component/CSAT_componentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSAT_componentApp.swift
+//  CSAT_component
+//
+//  Created by harsh chauhan on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CSAT_componentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
